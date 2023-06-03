395d9cf69623d7bed298c5d27b137563738935a0
PonGuru11 <125974004+PonGuru11@users.noreply.github.com>
2023-06-03 10:59:28 +0530

    Add files via upload

---
+++ api/controllers/userController.js
@@ -0,0 +1,262 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+  jwt = require('jsonwebtoken'),
+  bcrypt = require('bcrypt'),
+  User = mongoose.model('User'),
+  path = require('path'),
+  async = require('async'),
+  crypto = require('crypto'),
+  _ = require('lodash'),
+  hbs = require('nodemailer-express-handlebars'),
+  email = process.env.MAILER_EMAIL_ID || 'nathanguru2016@gmail.com',
+  pass = process.env.MAILER_PASSWORD || 'zbfjgxoxxjjetxsb',
+  nodemailer = require('nodemailer');
+
+
+var smtpTransport = nodemailer.createTransport({
+  service: process.env.MAILER_SERVICE_PROVIDER || 'Gmail',
+  auth: {
+    user: email,
+    pass: pass
+  }
+});
+
+
+var handlebarsOptions = {
+  viewEngine: 'handlebars',
+  viewPath: path.resolve('./api/templates/'),
+  extName: '.html'
+};
+
+smtpTransport.use('compile', hbs(handlebarsOptions));
+
+
+exports.register = function(req, res) {
+  var newUser = new User(req.body);
+  newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
+  newUser.save(function(err, user) {
+    if (err) {
+      return res.status(400).send({
+        message: err
+      });
+    } else {
+      user.hash_password = undefined;
+      return res.json(user);
+    }
+  });
+};
+
+exports.index = function(req, res) {
+  return res.sendFile(path.resolve('./public/home.html'));
+};
+
+exports.render_forgot_password_template = function(req, res) {
+  return res.sendFile(path.resolve('./public/forgot-password.html'));
+};
+
+exports.render_reset_password_template = function(req, res) {
+  return res.sendFile(path.resolve('./public/reset-password.html'));
+};
+
+exports.sign_in = function(req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+    if (!user || !user.comparePassword(req.body.password)) {
+      return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
+    }
+    return res.json({ token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 'RESTFULAPIs') });
+  });
+};
+
+exports.loginRequired = function(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    return res.status(401).json({ message: 'Unauthorized user!' });
+  }
+};
+
+// exports.forgot_password = function(req, res) {
+//   async.waterfall([
+//     function(done) {
+//       User.findOne({
+//         email: req.body.email
+//       }).exec(function(err, user) {
+//         if (user) {
+//           done(err, user);
+//         } else {
+//           done('User not found.');
+//         }
+//       });
+//     },
+//     function(user, done) {
+//       // create the random token
+//       crypto.randomBytes(20, function(err, buffer) {
+//         var token = buffer.toString('hex');
+//         done(err, user, token);
+//       });
+//     },
+//     function(user, token, done) {
+//       User.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function(err, new_user) {
+//         done(err, token, new_user);
+//       });
+//     },
+//     function(token, user, done) {
+//       var data = {
+//         to: user.email,
+//         from: email,
+//         template: 'forgot-password-email',
+//         subject: 'Password help has arrived!',
+//         context: {
+//           url: 'http://localhost:3000/auth/reset_password?token=' + token,
+//           name: user.fullName.split(' ')[0]
+//         }
+//       };
+
+//       smtpTransport.sendMail(data, function(err) {
+//         if (!err) {
+//           return res.send({ message: 'Kindly check your email for further instructions' ,data
+         
+//         });
+//         } else {
+//           return done(err);
+//         }
+//       });
+//     }
+//   ], function(err) {
+//     return res.status(422).json({ message:"Powww", err });
+//   });
+// };
+const handlebars = require('handlebars');
+const fs = require('fs');
+
+exports.forgot_password = function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({
+        email: req.body.email
+      }).exec(function(err, user) {
+        if (user) {
+          done(err, user);
+        } else {
+          done('User not found.');
+        }
+      });
+    },
+    function(user, done) {
+      // create the random token
+      crypto.randomBytes(20, function(err, buffer) {
+        var token = buffer.toString('hex');
+        done(err, user, token);
+      });
+    },
+    function(user, token, done) {
+      User.findByIdAndUpdate(
+        { _id: user._id },
+        {
+          reset_password_token: token,
+          reset_password_expires: Date.now() + 86400000
+        },
+        { upsert: true, new: true }
+      ).exec(function(err, new_user) {
+        done(err, token, new_user);
+      });
+    },
+    function(token, user, done) {
+      // Load the email template
+      fs.readFile('./api/templates/forgot-password-email.html', 'utf-8', function(err, emailTemplate) {
+        if (err) {
+          return done(err);
+        }
+
+        // Compile the template
+        const template = handlebars.compile(emailTemplate);
+const nurl = 'http://localhost:3000/auth/reset_password?token=';
+        // Render the email template with the context
+        const encodedToken = encodeURIComponent(token);
+        // Render the email template with the context
+        const emailContent = template({
+          url: nurl + encodedToken,
+          name: user.fullName.split(' ')[0]
+        });
+
+        var data = {
+          to: user.email,
+          from: email,
+          subject: 'Password help has arrived!',
+          html: emailContent
+        };
+
+        smtpTransport.sendMail(data, function(err) {
+          if (!err) {
+            return res.send({
+              message: 'Kindly check your email for further instructions',
+              data
+            });
+          } else {
+            return done(err);
+          }
+        });
+      });
+    }
+  ], function(err) {
+    return res.status(422).json({ message: 'Error', err });
+  });
+};
+
+
+/**
+ * Reset password
+ */
+exports.reset_password = function(req, res, next) {
+  User.findOne({
+    reset_password_token: req.body.token,
+    reset_password_expires: {
+      $gt: Date.now()
+    }
+  }).exec(function(err, user) {
+    if (!err && user) {
+      if (req.body.newPassword === req.body.verifyPassword) {
+        user.hash_password = bcrypt.hashSync(req.body.newPassword, 10);
+        user.reset_password_token = undefined;
+        user.reset_password_expires = undefined;
+        user.save(function(err) {
+          if (err) {
+            return res.status(422).send({
+              message: err
+            });
+          } else {
+            var data = {
+              to: user.email,
+              from: email,
+              template: 'reset-password-email',
+              subject: 'Password Reset Confirmation',
+              context: {
+                name: user.fullName.split(' ')[0]
+              }
+            };
+
+            smtpTransport.sendMail(data, function(err) {
+              if (!err) {
+                return res.json({ message: 'Password reset' });
+              } else {
+                return done(err);
+              }
+            });
+          }
+        });
+      } else {
+        return res.status(422).send({
+          message: 'Passwords do not match'
+        });
+      }
+    } else {
+      return res.status(400).send({
+        message: 'Password reset token is invalid or has expired.'
+      });
+    }
+  });
+};
